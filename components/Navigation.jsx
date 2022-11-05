dfc000e478d572594bed4c926f83061aed360f12
Barbara Szewczyk <comamysza@gmail.com>
2022-11-05 15:36:37 +0100

    Adds navigation

---
+++ components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navigation.css';
+
+export default function Navigation() {
+	return (
+		<nav className='nav'>
+			<figure className='logo'>
+				<img src='/images/travel.png' alt='Airplane flying over eartch icon' />
+			</figure>
+			<h3>Basia's travel journal</h3>
+		</nav>
+	);
+}
